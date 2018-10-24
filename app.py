ee5d886a508ed4d62e5875d92afa364b130d177d
Taisa <taisa@MacBook-Pro-de-Taisa-2.local>
2018-10-24 17:56:48 -0300

    hola

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 import pandas as pd
 import numpy as np
 
+## Establecer datasets
 df_lead = pd.read_csv('goalglue-sxkm-leads.csv',delimiter=",")
 df_goals = pd.read_csv('goalglue-sxkm-goals.csv',delimiter=",")
 
+df_lead['Source / Medium'] = df_lead['Source / Medium'].replace(to_replace='emma.cl.*|Seguros_Sura.*',value='emma / email', regex=True)
+
+## Mapear información y cambiar a str las columnas no númericas.
 df_lead['key'] = df_lead['Day of the month'].map(str) + 'h' + df_lead['Hour'].map(str) + df_lead['Device Category'] + df_lead['Browser'] + df_lead['Operating System'] + 'OS' + df_lead['Operating System Version'].map(str) + 'res' + df_lead['Screen Resolution'] 
 df_goals['key'] = df_goals['Day of the month'].map(str) + 'h' + df_goals['Hour'].map(str) + df_goals['Device Category'] + df_goals['Browser'] + df_goals['Operating System'] + 'OS' + df_goals['Operating System Version'].map(str) + 'res' + df_goals['Screen Resolution'] 
 
+df_lead['count'] = 1
+
+## Sacar keys que estan en los goals
 df_leadgoal = df_lead.loc[df_lead['key'].isin(df_goals['key'])]
-
-""" print('total goals ' + str(len(df_goals.index)))
-print('total leadgoals ' + str(len(df_leadgoal.index)))
-print(df_goals.key.nunique())
-print(df_leadgoal.key.nunique()) """
-
 df_goalscl = df_goals[['key']].copy()
-df_leadgoalcl = df_leadgoal[['Source / Medium','key']].copy()
 
-df_cd = pd.merge(df_leadgoalcl, df_goalscl, how='inner', on = 'key')
-print(df_leadgoalcl.groupby('key')['Source / Medium'].head(10))
+## agregar rows para poder quedar con la fuente que se repite más por key
+df_aggregate_leadgoal = df_leadgoal.groupby(['key','Source / Medium'],as_index=False).agg({'count': 'sum'})
+df_leadgoalcl = df_aggregate_leadgoal.sort_values('count', ascending=False).drop_duplicates(['Source / Medium','key']).copy()
+
+## randomizar filas y eliminar duplicados
+result = df_leadgoalcl[['Source / Medium','key']].sample(frac = 1).reset_index(drop=True).drop_duplicates('key').copy()
+
+## Totales para comparación
+total_goals = df_goals['key'].count()
+interpolated_goals = result.groupby('Source / Medium').count().sum().values
+
+print('Total Goals ' + str(total_goals))
+print('Interpolated Goals' + str(interpolated_goals))
+print('% of goals' + str(interpolated_goals/total_goals))
+print(result.groupby('Source / Medium').count())
+print(result.head(10))
