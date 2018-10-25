7467bdd170658fc382f255ba9ffc365e591aaeaf
Taisa <taisa@MacBook-Pro-de-Taisa-2.local>
2018-10-25 10:43:37 -0300

    sheets

---
+++ connect.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+import pandas as pd
+
+from private import SPREADSHEET_ID
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+# The ID and range of a sample spreadsheet.
+
+def main( spreadsheet_id, range_name ):
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+    # Call the Sheets API
+    RANGE_NAME = 'Goal!A15:I'
+    result = service.spreadsheets().values().get(spreadsheetId= spreadsheet_id,
+                                                range=range_name).execute()
+    values = result.get('values', [])
+    if not values:
+        return print('No hay datos')
+    else:
+        df_goals = pd.DataFrame(values, columns = values[0])
+        df_goals = df_goals.drop(df_goals.index[0])
+        return df_goals
+
+""" if __name__ == '__main__':
+    main() """
---
+++ private.py
@@ -0,0 +1,5 @@
+SPREADSHEET_ID = '1r8wOrQbdKlTgWr72x9pgGEnw4l4xh2qlFSK15ergT2s'
+SHEET_RANGES = {
+    'goal':'Goal!A:I',
+    'event':'Eventos!A:I'
+}
